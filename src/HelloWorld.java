35d0673d746d3c396e05ea3e0b8c1fc5fa188a4d
charlesrolo22 <charles.a.roland@gmail.com>
2023-04-21 14:21:49 -0500

    helloworldupdate

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,78 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int myFavoriteNumber = 7;
+        System.out.println("My favorite number is " + myFavoriteNumber);
+
+        String myString = "Hello, world!";
+        System.out.println(myString);
+
+
+        myString = 'c';
+        System.out.println(myString);
+
+
+        myString = 3.14159;
+
+        long myNumber;
+        System.out.println(myNumber);
+
+
+         myNumber = 3.14;  // This will result in a compile error because 3.14 is a double value and cannot be automatically converted to a long value
+
+
+        myNumber = 123L;
+        System.out.println(myNumber);
+
+
+        myNumber = 123;
+        System.out.println(myNumber);
+
+
+         myNumber = 3.14;
+
+         myNumber = 3.14f;
+         myNumber = (float) 3.14;
+
+
+
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+
+        int z = 5;
+        z = z + 5;
+        System.out.println(z);
+
+
+        int a = 5;
+        a += 5;
+        System.out.println(a);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        int three = (int) "three";
+
+        int x = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
     }
 }
---
+++ src/MethodsExercises.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+////
+//    public static int addition(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public static int subtraction(int a, int b) {
+//        return a - b;
+//    }
+//
+//    public static int multiplication(int a, int b) {
+//        return a * b;
+//    }
+//
+//    public static int division(int a, int b) {
+//            return a / b;
+//        }
+//    }
+//
+//    public static int modulus(int a, int b) {
+//        return a % b;
+//    }
+//
+//    public static void main(String[] args) {
+//        int a = 6;
+//        int b = 22;
+//        System.out.println("Addition: " + addition(a, b));
+//        System.out.println("Subtraction: " + subtraction(a, b));
+//        System.out.println("Multiplication: " + multiplication(a, b));
+//        System.out.println("Division: " + division(a, b));
+//        System.out.println("Modulus: " + modulus(a, b));
+//    }
+
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 100;
+        int validInput = getValidInput(min, max);
+        System.out.println("Valid input: " + validInput);
+        System.out.println(factorial());
+    }
+
+    public static int getValidInput(int min, int max) {
+
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.print("Enter a number between " + min + " and " + max + ": ");
+            String inputString = scanner.nextLine();
+            try {
+                input = Integer.parseInt(inputString);
+                if (input >= min && input <= max) {
+                    validInput = true;
+                } else {
+                    System.out.println("Error: input must be between " + min + " and " + max);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: input must be a valid integer");
+            }
+        }
+        return input;
+
+
+
+
+    public static long factorial(){
+        int userFactorial = getValidInput(1, 10);
+        long startNumber = 1;
+        for(int i = 1; i <= userFactorial; i++){
+            startNumber *= i;
+        }
+    }
+
+}
+
+public static boolean userContinue(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to continue? (y/n)");
+        String answer = scanner.nextLine();
+        if (answer.equals("y")){
+            return true;
+
+        }
+    }
+
+    public static int dice(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("how many sides do your dice has?");
+        int sides = scanner.nextInt();
+        int dice1 = (int) ((Math.random() * sides) * 1);
+        int dice2 = (int) ((Math.random() * sides) * 1);
+
+        return dice1 + dice2;
+    }
+
+}
