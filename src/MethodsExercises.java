0781b3b1c1a031329c5a9f86d44cd933e58f4868
charlesrolo22 <charles.a.roland@gmail.com>
2023-04-24 11:44:23 -0500

    update

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
-
-public class MethodsExercises {
+//import java.util.Scanner;
+//
+//public class MethodsExercises {
 ////
 //    public static int addition(int a, int b) {
 //        return a + b;
@@ -33,66 +33,66 @@ public class MethodsExercises {
 //        System.out.println("Modulus: " + modulus(a, b));
 //    }
 
-    public static void main(String[] args) {
-        int min = 1;
-        int max = 100;
-        int validInput = getValidInput(min, max);
-        System.out.println("Valid input: " + validInput);
-        System.out.println(factorial());
-    }
-
-    public static int getValidInput(int min, int max) {
-
-        Scanner scanner = new Scanner(System.in);
-        int input = 0;
-        boolean validInput = false;
-        while (!validInput) {
-            System.out.print("Enter a number between " + min + " and " + max + ": ");
-            String inputString = scanner.nextLine();
-            try {
-                input = Integer.parseInt(inputString);
-                if (input >= min && input <= max) {
-                    validInput = true;
-                } else {
-                    System.out.println("Error: input must be between " + min + " and " + max);
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Error: input must be a valid integer");
-            }
-        }
-        return input;
-
-
-
-
-    public static long factorial(){
-        int userFactorial = getValidInput(1, 10);
-        long startNumber = 1;
-        for(int i = 1; i <= userFactorial; i++){
-            startNumber *= i;
-        }
-    }
-
-}
-
-public static boolean userContinue(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Would you like to continue? (y/n)");
-        String answer = scanner.nextLine();
-        if (answer.equals("y")){
-            return true;
-
-        }
-    }
-
-    public static int dice(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("how many sides do your dice has?");
-        int sides = scanner.nextInt();
-        int dice1 = (int) ((Math.random() * sides) * 1);
-        int dice2 = (int) ((Math.random() * sides) * 1);
-
-        return dice1 + dice2;
-    }
-
+//    public static void main(String[] args) {
+//        int min = 1;
+//        int max = 100;
+//        int validInput = getValidInput(min, max);
+//        System.out.println("Valid input: " + validInput);
+//        System.out.println(factorial());
+//    }
+//
+//    public static int getValidInput(int min, int max) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//        int input = 0;
+//        boolean validInput = false;
+//        while (!validInput) {
+//            System.out.print("Enter a number between " + min + " and " + max + ": ");
+//            String inputString = scanner.nextLine();
+//            try {
+//                input = Integer.parseInt(inputString);
+//                if (input >= min && input <= max) {
+//                    validInput = true;
+//                } else {
+//                    System.out.println("Error: input must be between " + min + " and " + max);
+//                }
+//            } catch (NumberFormatException e) {
+//                System.out.println("Error: input must be a valid integer");
+//            }
+//        }
+//        return input;
+//
+//
+//
+//
+//    public static long factorial(){
+//        int userFactorial = getValidInput(1, 10);
+//        long startNumber = 1;
+//        for(int i = 1; i <= userFactorial; i++){
+//            startNumber *= i;
+//        }
+//    }
+//
+//}
+//
+//public static boolean userContinue(){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Would you like to continue? (y/n)");
+//        String answer = scanner.nextLine();
+//        if (answer.equals("y")){
+//            return true;
+//
+//        }
+//    }
+//
+//    public static int dice(){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("how many sides do your dice has?");
+//        int sides = scanner.nextInt();
+//        int dice1 = (int) ((Math.random() * sides) * 1);
+//        int dice2 = (int) ((Math.random() * sides) * 1);
+//
+//        return dice1 + dice2;
+//    }
+//
 }
