4a68153fc1349048bdb07e1af875a2f1c8e66306
charlesrolo22 <charles.a.roland@gmail.com>
2023-04-26 11:08:08 -0500

    shapes

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,13 +2,24 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println("Box 1 perimeter: " + box1.getPerimeter());
-        System.out.println("Box 1 area: " + box1.getArea());
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println("Box 1 perimeter: " + box1.getPerimeter());
+//        System.out.println("Box 1 area: " + box1.getArea());
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println("Box 2 perimeter: " + box2.getPerimeter());
+//        System.out.println("Box 2 area: " + box2.getArea());
 
-        Rectangle box2 = new Square(5);
-        System.out.println("Box 2 perimeter: " + box2.getPerimeter());
-        System.out.println("Box 2 area: " + box2.getArea());
+        Measurable myShape = new Square(4);
+
+
+        Measurable yourShape = new Rectangle(6, 10);
+
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+
+        System.out.println(yourShape.getArea());
+        System.out.println(yourShape.getPerimeter());
     }
 }
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,17 +1,30 @@
 package shapes;
 
-public class Square extends Rectangle {
-    public Square(double side) {
-        super(side, side);
+public class Square extends Quadrilateral {
+
+    public Square(double length) {
+        super(length, length);
     }
 
-
-    public double getArea() {
-        return Math.pow(length, 2);
-    }
-
-
+    @Override
     public double getPerimeter() {
         return 4 * length;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+        this.width = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
+        this.length = width;
     }
 }
